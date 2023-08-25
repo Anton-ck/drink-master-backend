b997e9c841301bf88fc278838608f08381cfc8eb
Nicholas <nicholasmoskalenko@gmail.com>
2023-08-26 00:09:07 +0300

    add recipes
    model + controller + routes

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -1,7 +1,11 @@
 import express from "express";
+import authenticate from "../../middlewares/authenticate.js";
+import controllers from "../../controllers/recipes.js";
 
 const router = express.Router();
 
-router.get("/");
+router.get("/category-list", authenticate, controllers.getCategories);
+router.get("/main-page", authenticate, controllers.getCocktailsForMain);
+router.get("/:param", authenticate, controllers.getCocktailParam);
 
 export default router;

1485071d9306a05ea1b904f2d9f7bbd00c48b902
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-08-27 11:52:43 +0300

    delete unuse subscribe model

--- routes/api/subscribe.js
+++ routes/api/subscribe.js
@@ -1,16 +1,12 @@
 import express from "express";
 
-import {
-
-  userEmailSchema,
-} from "../../schemas/subscribe.js";
+import { userEmailSchema } from "../../schemas/subscribe.js";
 
 import validateBody from "../../middlewares/validateBody.js";
 import isEmptyBody from "../../middlewares/isEmptyBody.js";
 import authenticate from "../../middlewares/authenticate.js";
 
 import controllers from "../../controllers/subscribe.js";
-
 
 const router = express.Router();
 

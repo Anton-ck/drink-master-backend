5df7022e90f2c28be376ca1adf9b587084c6058a
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-08-31 00:07:53 +0300

    deleted unuse files

--- models/ingredients.js
+++ models/ingredients.js
@@ -1,6 +1,5 @@
 import { Schema, model } from "mongoose";
 
-// import handleMongooseError from "../helpers/handleMongooseError.js";
 
 const ingredientSchema = new Schema(
 	{
--- models/search.js
+++
@@ -1,3 +0,0 @@
-import { Schema, model } from "mongoose";
-
-import handleMongooseError from "../helpers/handleMongooseError.js";

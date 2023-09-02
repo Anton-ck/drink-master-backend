711ea04a452d8b94abf5c6329230b1f2c48578ac
Irina Iskra <iskrairina.a@gmail.com>
2023-09-02 20:27:44 +0300

    required

--- schemas/cocktails.js
+++ schemas/cocktails.js
@@ -24,7 +24,7 @@ export const cocktailSchema = Joi.object({
   instructionsRU: Joi.string(),
   instructionsPL: Joi.string(),
   instructionsUK: Joi.string(),
-  drinkThumb: Joi.string().required(),
+  drinkThumb: Joi.string(),
   // ingredients: Joi.array().items(Joi.string().required()).min(2).required(),
   ingredients: Joi.array()
     .items(

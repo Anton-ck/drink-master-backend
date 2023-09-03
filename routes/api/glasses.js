a0f3cb18f6c07747a4a96f7c1e4227fc8f6205a9
MargaritaSukhova <margarytasu@gmail.com>
2023-09-03 22:05:12 +0300

    fix

--- routes/api/glasses.js
+++ routes/api/glasses.js
@@ -1,7 +1,9 @@
 import express from "express";
 import controllers from "../../controllers/glasses.js";
+import authenticate from "../../middlewares/authenticate.js";
+
 const glassesRouter = express.Router();
 
-glassesRouter.get("/", controllers.getGlasses);
+glassesRouter.get("/", authenticate, controllers.getGlasses);
 
 export default glassesRouter;
--- routes/api/ingredients.js
+++ routes/api/ingredients.js
@@ -1,11 +1,9 @@
 import express from "express";
-
+import authenticate from "../../middlewares/authenticate.js"
 import controllers from "../../controllers/ingredients.js";
 
 const router = express.Router();
 
-router.get("/list", controllers.getIngredientsList);
-
-// router.get('/', authenticate, getReceiptByIngredient)
+router.get("/list", authenticate, controllers.getIngredientsList);
 
 export default router;

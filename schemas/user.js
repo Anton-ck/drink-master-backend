26b651fadd5cd21479b9da42e1e62e42fadf3fc8
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-09-03 22:30:51 +0300

    email regexp

--- schemas/user.js
+++ schemas/user.js
@@ -5,12 +5,22 @@ import emailRegexp from "../helpers/regExp.js";
 
 export const registerSchema = Joi.object({
   name: Joi.string().min(3).max(16).required(),
-  email: Joi.string().pattern(new RegExp(emailRegexp)).required(),
+  email: Joi.string().pattern(emailRegexp).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format mail@mail.com.",
+  }),
   password: joiPassword.string().minOfLowercase(1).minOfUppercase(1).minOfNumeric(1).required(),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().pattern(new RegExp(emailRegexp)).required(),
+  email: Joi.string().pattern(emailRegexp).required().empty(false).messages({
+    "string.base": "The email must be a string.",
+    "any.required": "The email field is required.",
+    "string.empty": "The email must not be empty.",
+    "string.pattern.base": "The email must be in format mail@mail.com.",
+  }),
   password: joiPassword.string().minOfLowercase(1).minOfUppercase(1).minOfNumeric(1).required(),
 });
 
